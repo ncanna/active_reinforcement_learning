364e7da434ac88d58f8425ef26a02de497f0cbf7
Navya Annapareddy <na3au@virginia.edu>
2021-03-29 17:08:24 -0400

    Delete Repo directory

--- Repo/hpc_pathing.py
+++
@@ -1,8 +0,0 @@
-import os, time
-
-# Check what the main folder is on rivanna
-main_folder = os.path.dirname(os.path.abspath(__file__))
-time.sleep(5)
-print(main_folder)
-# os.makedirs("/scratch/nm4wu/modelRuns")
-
